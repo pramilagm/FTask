fc627e6fe818f72e9cbcf2830b4deb13f259b027
pramilagm <pmiris.pramila@gmail.com>
2020-08-06 11:43:45 -0700

    basic operation on SLL

--- LinkedList/SinglyLL/insertion.py
+++ LinkedList/SinglyLL/insertion.py
@@ -34,12 +34,58 @@ class SinglyLL:
             current = current.next
         print(count)
 
+# iterative solution to find max element
+    def maxElement(self):
+        max = self.head.data
+        current = self.head
+        while current.next is not None:
+            current = current.next
+            if current.data > max:
+                max = current.data
+
+        return max
+
+    def getCountRecur(self, node):
+        if node is None:
+            return 0
+        return 1 + self.getCountRecur(node.next)
+
+    def findCount(self):
+        return self.getCountRecur(self.head)
+
+# recursive solution to find max element
+    # def maxElemRecursive(self):
+    def getMaxRecur(self, node):
+        # base case
+        if node.next is None:
+            return node.data
+        return max(node.next.data, self.getMaxRecur(node.next))
+
+    def findMaxRecur(self):
+        return self.getMaxRecur(self.head)
+# to find first repeated elements
+
+    def findRepeatedElements(self):
+        current = self.head
+
+        while current.next:
+            if current.data == current.next.data:
+                return current.data
+            current = current.next
+        return -1
+
 
 llist = SinglyLL()
 # llist.head = Node(3)
-llist.push(5)
+llist.push(12)
 llist.push(8)
-llist.push(8)
-llist.push(7)
+llist.push(18)
+llist.push(4)
+llist.push(4)
+llist.push(13)
 # llist.printList()
 llist.countElement()
+print(llist.maxElement())
+print(llist.findCount())
+print(llist.findMaxRecur())
+print(llist.findRepeatedElements())
